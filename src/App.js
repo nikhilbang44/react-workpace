ba08f1aaf20a50388ff7dac16f905d4b4b0fcccb
Nikhil Bang <nikhilbang44@gmail.com>
2018-11-21 15:11:53 +0530

    Props

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React APP</h1>
-        <p>This is really working!</p>
-        <Person/>
+        <h1> Hi, I'm a React APP </h1>
+        <p> This is really working! </p>
+        < Person name="max" age="28" />
+        < Person name = "mak" age = "28" />
+        < Person name = "pavan" age = "28" />
       </div>
     );
   }
