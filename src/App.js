73c225ad3812778113526c3f2f3352f7f9a451ce
Nikhil Bang <nikhilbang44@gmail.com>
2018-11-26 17:44:37 +0530

    state and lists

--- src/App.js
+++ src/App.js
@@ -60,6 +60,12 @@ class App extends Component {
     })
   }
 
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
 
   render() {
 
@@ -76,8 +82,9 @@ class App extends Component {
     if ( this.state.showPersons) {
       persons = (
         <div>
-          { this.state.persons.map(person => { 
+          { this.state.persons.map((person, index) => {
               return <Person
+              click={() => this.deletePersonHandler(index)}
               name = { person.name }
               age = { person.age }
               />
