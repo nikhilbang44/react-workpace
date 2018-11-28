b67d25f7f05a21297992281b09420848e0e16b37
Nikhil Bang <nikhilbang44@gmail.com>
2018-11-28 17:05:18 +0530

    Enabling & using CSS modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -110,20 +110,20 @@ class App extends Component {
       );
     }
 
-    let classes = [];
+    let assigendClasses = [];
 
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assigendClasses.push(classes.red);
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assigendClasses.push(classes.bold);
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1> Hi, I'm a React APP </h1>
-          <p className= {classes.join(' ')}> This is really working! </p>
+          <p className= {assigendClasses.join(' ')}> This is really working! </p>
           <button style= {style} onClick={this.togglePersonsHandler}> Toggle Person</button>
           { persons }
         </div>
